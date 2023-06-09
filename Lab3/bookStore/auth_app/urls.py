f40d1c8450173775cf917d158cef33cd57a0be43
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-06-10 02:56:33 +0300

    .

--- Lab3/bookStore/auth_app/urls.py
+++
@@ -1,9 +0,0 @@
-#auth urls
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login',views.loginPg , name='login'),
-    path('signup',views.signupPg , name='signup'),
-    path('signout',views.signoutPg , name='signout')
-]
--- Lab3/bookStore/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    # return plain text
-    return render(request,'main/home.html')
